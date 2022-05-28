d2a6202315799ead859d09713f62b68efa7ecb83
Mihai Enuica <mihai.enuica@gmail.com>
2022-05-28 19:24:48 +0300

    Deploying Smart Contracts & Running a Local Blockchain

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from 'web3modal'
+
 
 export default function Home() {
   return (
