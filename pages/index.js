2385c6da75e1a7d5e30fe3a2427b6867584340ac
Mihai Enuica <mihai.enuica@gmail.com>
2022-06-01 11:56:01 +0300

    Load NFTs to the Market

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,58 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from 'web3modal'
 
+import {nftMarketAddress, nftAddress} from '../config.js'
+import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
+import KBMarket from '../artifacts/contracts/KBMarket.sol/KBMarket.json'
+
 
 export default function Home() {
+  const [nfts, setNFTs] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
+
+
+  useEffect(() =>{
+    loadNFTs()
+  }, [])
+
+
+  async function loadNFTs() {
+    // what we want to load:
+    // ***provider, tokenContract, marketContract, data for our marketItems***
+
+    // get the connection with blockchain
+    const provider = new ethers.providers.JsonRpcProvider()
+    // get the nftContract and marketContract
+    const tokenContract = new ethers.Contract(nftAddress, NFT.abi, provider)
+    const marketContract = new ethers.Contract(nftMarketAddress, KBMarket.abi, provider)
+    // get all the tokens - nfts
+    const data = await marketContract.fetchMarketTokens()
+
+    const items = await Promise.all(data.map(async i => {
+      // get the tokenUri
+      const tokenUri = await tokenContract.tokenURI(i.tokenId)
+      // we want get the token metadata - json from the nft URI
+      const meta = await axios.get(tokenUri)
+
+      let price = ethers.utils.formatUnits(i.price.toString(), 'ether')
+
+      let item = {
+        price,
+        tokenId: i.tokenId.toNumber(),
+        seller: i.seller,
+        owner: i.owner,
+        image: meta.data.image, 
+        name: meta.data.name,
+        description: meta.data.description
+      }
+      return item
+    }))
+
+    setNFTs(items)
+    setLoadingState('loaded')
+  }
+
+
   return (
     <div>
       
